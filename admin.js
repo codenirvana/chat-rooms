4c76a7c1797cf452735ebff8daf480dad8e4b7e5
Udit <codenirvana28@gmail.com>
2016-05-29 21:28:28 +0530

    clear routes

--- admin.js
+++ admin.js
@@ -7,7 +7,7 @@ var rooms = require('./data/rooms.json')
 var router = express.Router();
 module.exports = router;
 
-router.get('/rooms', function (req, res) {
+router.get('/rooms', function(req, res) {
     res.render("rooms", {
         baseUrl: req.baseUrl,
         title: "Rooms",
@@ -15,57 +15,58 @@ router.get('/rooms', function (req, res) {
     });
 });
 
-router.get('/rooms/add', function (req, res) {
-    res.render("add", {
-        baseUrl: req.baseUrl,
-        title: "Add Room"
+router.route('/rooms/add')
+    .get(function(req, res) {
+        res.render("add", {
+            baseUrl: req.baseUrl,
+            title: "Add Room"
+        });
+    })
+    .post(function(req, res) {
+        var room = {
+            name: req.body.name,
+            id: uuid.v4()
+        };
+
+        rooms.push(room);
+
+        res.redirect(req.baseUrl + '/rooms');
     });
-});
 
-router.post('/rooms/add', function (req, res) {
-    var room = {
-        name: req.body.name,
-        id: uuid.v4()
-    };
+router.route('/rooms/edit/:id')
+    .get(function(req, res) {
+        var id = req.params.id;
+        var room = _.find(rooms, r => r.id === id);
 
-    rooms.push(room);
+        if (!room) {
+            res.sendStatus(404);
+            return;
+        }
 
-    res.redirect(req.baseUrl + '/rooms');
-});
+        res.render('edit', {
+            baseUrl: req.baseUrl,
+            title: "Edit Room",
+            room
+        });
+    })
+    .post(function(req, res) {
+        var id = req.params.id;
+        var room = _.find(rooms, r => r.id === id);
 
-router.get('/rooms/delete/:id', function (req, res) {
+        if (!room) {
+            res.sendStatus(404);
+            return;
+        }
+
+        room.name = req.body.name;
+
+        res.redirect(req.baseUrl + '/rooms');
+    });
+
+router.get('/rooms/delete/:id', function(req, res) {
     var id = req.params.id;
 
     rooms = rooms.filter(r => r.id !== id);
-
-    res.redirect(req.baseUrl + '/rooms');
-});
-
-router.get('/rooms/edit/:id', function (req, res) {
-    var id = req.params.id;
-    var room = _.find(rooms, r => r.id === id);
-
-    if(!room) {
-        res.sendStatus(404);
-        return;
-    }
-
-    res.render('edit', {
-        baseUrl: req.baseUrl,
-        title: "Edit Room", room
-    });
-});
-
-router.post('/rooms/edit/:id', function (req, res) {
-    var id = req.params.id;
-    var room = _.find(rooms, r => r.id === id);
-
-    if(!room) {
-        res.sendStatus(404);
-        return;
-    }
-
-    room.name = req.body.name;
 
     res.redirect(req.baseUrl + '/rooms');
 });
